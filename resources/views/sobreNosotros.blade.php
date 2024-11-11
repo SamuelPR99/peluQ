719718f83410ca6305529d2230a2245895188c72
HugoSanchezS <145431439+HugoSanchezS@users.noreply.github.com>
2024-11-11 14:03:18 +0100

    aaa

--- resources/views/sobreNosotros.blade.php
+++ resources/views/sobreNosotros.blade.php
@@ -6,12 +6,14 @@
     <title>Sobre nosotros</title>
     @vite(['resources/css/app.css', 'resources/js/app.js']) <!--aqui se declara el tailwind de los cojones-->
 </head>
+<x-cuerpo-pagina />
 <body>
-    <h1>Sobre nosotros</h1>
-    <div class="mt-10 box-border h-100 w-100 p-8 border-4">
+    <h1 class="text-center mt-10 italic text-4xl antialiased font-bold drop-shadow-md hover:scale-150 transition 1">Sobre nosotros</h1>
+    <div class="mt-10 box-content h-120 w-120 p-4 border-4">
         <p>Esta es la página de Acerca de. Aquí puedes incluir información sobre tu aplicación.</p>
         <p>Agrega más detalles sobre tu proyecto, su propósito, y cualquier otra información relevante.</p>
-        <a href="{{ url('/') }}">Volver a la página de inicio</a>
+        <button onclick="window.location= '{{ url('/') }}'" class="text-cyan-500 underline hover:scale-110 transition 1" >Volver a la página de inicio</button>
     </div>
 </body>
+<x-pie-pagina />
 </html>
