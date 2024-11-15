9b949b1ea9bde02ae1084da1c8978a23f0d09d1a
Enrique Lopez <enriquelopezrodriguez14@gmail.com>
2024-11-15 13:37:11 +0100

    Mas cambios esteticos

--- resources/views/citas/create.blade.php
+++ resources/views/citas/create.blade.php
@@ -29,8 +29,14 @@
                     </select>
                 </div>
                 <input type="hidden" name="estado_cita" value="pendiente">
-                <button type=S"submit" class="btn btn-primary bg-blue-500 text-white px-4 py-2 rounded-md">Crear Cita</button>
+                <div class="justify-center flex">
+                    <button type="submit" class="btn btn-primary bg-white hover:bg-red-500 text-gray-800 font-bold py-2 px-4 rounded transition ease-in-out duration-150">Crear Cita</button>
+                </div>
             </form>
+            <div id="leyenda" class="z-20 bg-gray-800 bg-opacity-70 p-4 rounded-lg shadow-md max-w-xs absolute right-12 mr-20 top-60 hidden">
+                <h2 class="text-white text-lg font-bold mb-2">Información</h2>
+                <p id="leyenda-contenido" class="text-white">Pasa el mouse sobre el mapa para ver información.</p>
+            </div>
         </div>
     </div>
 
@@ -38,33 +44,57 @@
         let map;
         let markers = [];
         let empresas = @json($empresas);
+        
         function initMap() {
-    map = L.map('map').setView([40.416, -3.70], 5);
-
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-
-    empresas.forEach(empresa => {
-        let marker = L.marker([parseFloat(empresa.coordenadas.split(',')[0]), parseFloat(empresa.coordenadas.split(',')[1])]).addTo(map)
-            .bindPopup(`
-                <div class="bg-gray-800 p-4 rounded-lg shadow-lg">
-                    <h3 class="text-lg font-bold text-red-700">${empresa.nombre_empresa}</h3>
-                    <p class="text-white">${empresa.direccion}</p>
-                    <p class="text-white">${empresa.telefono}</p>
-                </div>
-            `)
-            .on('click', function() {
-                document.getElementById('empresa_id').value = empresa.id;
-                document.getElementById('empresa-info').innerHTML = `
-                    <h3 class="text-lg font-bold text-white"> Cita para ${empresa.nombre_empresa}</h3>
-                `;
-                fetchPeluqueros(empresa.id);
+            map = L.map('map').setView([40.416, -3.70], 5);
+            
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {}).addTo(map);
+            
+            var customIcon = L.icon({
+                iconUrl: '/img/peluqueria.png',
+                iconSize: [30, 30],
+                iconAnchor: [22, 38],
+                popupAnchor: [-3, -38]
             });
 
-        markers.push(marker);
-    });
-}
+            empresas.forEach(empresa => {
+                let marker = L.marker([parseFloat(empresa.coordenadas.split(',')[0]), parseFloat(empresa.coordenadas.split(',')[1])], { icon: customIcon }).addTo(map)
+                    .bindPopup(`
+                        <div class="bg-gray-800 p-4 rounded-lg shadow-lg">
+                            <h3 class="text-lg font-bold text-red-700">${empresa.nombre_empresa}</h3>
+                            <p class="text-white">${empresa.direccion}</p>
+                            <p class="text-white">${empresa.telefono}</p>
+                        </div>
+                    `)
+                    .on('click', function() {
+                        document.getElementById('empresa_id').value = empresa.id;
+                        document.getElementById('empresa-info').innerHTML = `
+                            <h3 class="text-lg font-bold text-white"> Cita para ${empresa.nombre_empresa}</h3>
+                        `;
+                        fetchPeluqueros(empresa.id);
+                    })
+                    .on('mouseover', function() {
+                        document.getElementById('leyenda').classList.remove('hidden');
+                        document.getElementById('leyenda-contenido').innerHTML = `<div class="flex items-center"><img src="/img/pelu.png" class="w-7 h-7 mr-2">Peluqueria.</div>
+                                                                                  <div class="flex items-center"><img src="/img/bar.png" class="w-7 h-7 mr-2">Barbería.</div>
+                                                                                  <div class="flex items-center"><img src="/img/peluqueria.png" class="w-7 h-7 mr-2">Barbería y peluqueria.</div>`;
+                    })
+                    .on('mouseout', function() {
+                        document.getElementById('leyenda').classList.add('hidden');
+                    });
+
+                markers.push(marker);
+            });
+
+            // Eventos para el mapa
+            map.on('mouseover', function() {
+                document.getElementById('leyenda').classList.remove('hidden');
+            });
+
+            map.on('mouseout', function() {
+                document.getElementById('leyenda').classList.add('hidden');
+            });
+        }
 
         function fetchPeluqueros(empresaId) {
             fetch(`/api/empresas/${empresaId}/peluqueros`)
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,7 +6,7 @@
                     <h3 class="text-lg font-semibold">{{ __('Bienvenido, ') }} {{ Auth::user()->name }}</h3>
                     @if(Auth::user()->user_type == 'empresario')
                         <h4 class="mt-4"></h4>
-                        <div class="bg-gray-600 p-4 rounded-lg shadow-lg mb-7">
+                        <div class="bg-gray-600 p-4 rounded-lg mb-7 shadow-inner">
                             <h4 class="text-lg font-semibold mb-2">{{ __('Detalles de la Empresa') }}</h4>
                             <ul class="list-disc pl-5">
                                 <li><strong>{{ __('Nombre:') }}</strong> {{ Auth::user()->empresas->first()->nombre_empresa }}</li>
@@ -16,7 +16,7 @@
                             <a href="{{ route('empresas.edit', Auth::user()->empresas->first()->id) }}" class="inline-block px-4 py-2 mt-2 bg-white hover:bg-red-500 text-gray-800 font-bold py-2 px-4 rounded transition ease-in-out duration-150">{{ __('Editar Datos de la Empresa') }}</a>
                         </div>     
                             <div class="bg-gray-600 p-4 rounded-lg shadow-lg mt-7 mb-7">
-                                <h4 class="mt-4">{{ __('Valoraciones de la Empresa') }}</h4>
+                                <h4 class="mt-4"><strong>{{ __('Valoraciones de la Empresa') }}</strong></h4>
                                 @if(Auth::user()->empresas->first()->valoracion->isEmpty())
                                 <p>{{ __('No tienes valoraciones.') }}</p>
                                 @else
@@ -25,7 +25,7 @@
                                 @endif
                             </div>
                     <div class="bg-gray-600 p-4 rounded-lg shadow-lg mb-7">            
-                        <h4 class="mt-4">{{ __('Citas Programadas') }}</h4>
+                        <h4 class="mt-4"><strong>{{ __('Citas Programadas') }}</strong></h4>
                         @if(Auth::user()->citas->isEmpty())
                         <p>{{ __('No tienes citas programadas.') }}</p>
                         <a href="{{ route('citas.index') }}" class="inline-block px-4 py-2 mt-2 bg-white hover:bg-red-500 text-gray-800 font-bold py-2 px-4 rounded transition ease-in-out duration-150">{{ __('Pedir cita') }}</a>
@@ -44,7 +44,7 @@
                             
                             @if((Auth::user()->user_type == 'user' || Auth::user()->user_type == 'empresario') && Auth::user()->empresas->isEmpty())
                             <h4 class="mt-4">{{ __('¿Tienes una peluquería / barbería?') }}</h4>
-                            <a href="{{ route('empresas.index') }}" class="inline-block px-4 py-2 mt-2bg-white hover:bg-red-500 text-gray-800 font-bold py-2 px-4 rounded transition ease-in-out duration-150">{{ __('Dar de alta') }}</a>
+                            <a href="{{ route('empresas.index') }}" class="inline-block px-4 py-2 mt-2 bg-white hover:bg-red-500 text-gray-800 font-bold py-2 px-4 rounded transition ease-in-out duration-150">{{ __('Dar de alta') }}</a>
                             @endif
                             
                             @if(Auth::user()->user_type == 'admin')
