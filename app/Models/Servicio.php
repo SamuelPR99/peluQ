4c73e5a8457fa6a6ced718f0d6893a5586445eae
SamuelPR99 <145430725+SamuelPR99@users.noreply.github.com>
2024-11-16 20:56:29 +0100

    Servicios

---
+++ app/Models/Servicio.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Servicio extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'empresa_id',
+        'servicio',
+        'precio',
+    ];
+
+    public function empresa()
+    {
+        return $this->belongsTo(Empresa::class);
+    }
+}
