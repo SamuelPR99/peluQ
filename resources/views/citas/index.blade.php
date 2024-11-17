2d550220d60c30a8ba91b7f8d70adaab5d0d9796
SamuelPR99 <145430725+SamuelPR99@users.noreply.github.com>
2024-11-17 19:43:27 +0100

    fix

--- resources/views/citas/index.blade.php
+++ resources/views/citas/index.blade.php
@@ -1,7 +1,8 @@
-<x-app-layout>
+@extends('layouts.app')
+@section('content')
     <div class="container">
         <h1>Beneficios de Pedir Cita</h1>
         <p>Explicación breve sobre los beneficios de pedir cita a través de esta aplicación...</p>
         <a href="{{ route('citas.create') }}" class="btn btn-primary">Continuar</a>
     </div>
-</x-app-layout>
+@endsection
