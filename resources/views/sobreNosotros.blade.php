f9cf6708eed375193003b09356886dbb64bfada7
HugoSanchezS <145431439+HugoSanchezS@users.noreply.github.com>
2024-11-18 11:07:21 +0100

    a

--- resources/views/sobreNosotros.blade.php
+++ resources/views/sobreNosotros.blade.php
@@ -1,9 +1,24 @@
 @extends('layouts.app')
 @section('content')
     <h1 class="text-center mt-10 italic text-4xl antialiased font-bold drop-shadow-md hover:scale-150 transition 1">Sobre nosotros</h1>
-    <div class="mt-10 box-content h-120 w-120 p-4 border-4">
-        <p>Esta es la página de Acerca de. Aquí puedes incluir información sobre tu aplicación.</p>
-        <p>Agrega más detalles sobre tu proyecto, su propósito, y cualquier otra información relevante.</p>
-        <button onclick="window.location= '{{ url('/') }}'" class="text-cyan-500 underline hover:scale-110 transition 1" >Volver a la página de inicio</button>
+    <div class="mt-10 backdrop-blur-sm w-full max-w-lg p-4 space-y-2 bg-gray-800 bg-opacity-70 rounded-lg shadow-xl z-10 grid justify-items-center">
+        <p class="text-neutral-50">
+            
+            Esta es la página de Acerca de. Aquí puedes incluir información sobre tu aplicación.
+        
+            Agrega más detalles sobre tu proyecto, su propósito, y cualquier otra información relevante.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu enim suscipit, tempus magna vitae, molestie lacus. 
+            Curabitur in lorem ac dui ornare fermentum sit amet eu elit. 
+            Suspendisse potenti. Ut euismod malesuada mauris ut vulputate. 
+            Sed eget rutrum dui, non luctus ipsum. Duis interdum in sapien nec hendrerit. 
+            Integer dignissim vulputate consequat. 
+            Quisque a leo lacinia, fringilla ex non, lacinia justo.
+
+        </p>
     </div>
+
+    <div class="flex justify-center">
+    <button onclick="window.location= '{{ url('/dashboard') }}'" class="text-neutral-50 underline hover:scale-110 transition 1" >Volver a la página de inicio</button>    
+    </div>
+
 @endsection
