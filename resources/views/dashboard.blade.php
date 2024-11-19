fb4fd239fd22eaa3386ed2bee9274123766b2195
SamuelPR99 <spenareyes@gmail.com>
2024-11-19 13:24:00 +0100

    arreglo dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -77,16 +77,18 @@
                         <x-valoraciones :valoraciones="Auth::user()->valoracion" />
                         @endif
                     </div>                
+                    @if((Auth::user()->user_type == 'user' || Auth::user()->user_type == 'empresario') && Auth::user()->empresas->isEmpty())
                     <div class="bg-gray-600 p-4 rounded-lg mb-7 text-gray-200 mt-0 shadow-inner hover:shadow-red-600 transition-transform ease-in-out">                
-                        @if((Auth::user()->user_type == 'user' || Auth::user()->user_type == 'empresario') && Auth::user()->empresas->isEmpty())
                         <h4 class="mt-1"><strong>{{ __('¿Tienes una peluquería / barbería?') }}</strong></h4>
                         <a href="{{ route('empresas.index') }}" class="inline-block px-4 py-2 mt-2 bg-white hover:bg-green-500 text-gray-800 font-bold py-2 px-4 rounded transition ease-in-out duration-150">{{ __('Dar de alta') }}</a>
-                        @endif
-                        @if(Auth::user()->user_type == 'admin')
+                    </div>
+                    @endif
+                    @if(Auth::user()->user_type == 'admin')
+                    <div class="bg-gray-600 p-4 rounded-lg mb-7 text-gray-200 mt-0 shadow-inner hover:shadow-red-600 transition-transform ease-in-out">                
                         <h4 class="mt-4">{{ __('Acciones Administrativas') }}</h4>
                         <x-admin-actions />
-                        @endif
-                    </div>                
+                    </div>
+                    @endif
                     @if(Auth::user()->user_type == 'peluquero')
                     <h4 class="mt-4">{{ __('Cuadrante') }}</h4>
                     <x-cuadrante :cuadrante="Auth::user()->cuadrante" />
