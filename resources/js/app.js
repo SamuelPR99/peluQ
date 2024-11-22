770f6ac856ee905085b9b71c78432e95c72dde21
Enrique Lopez <enriquelopezrodriguez14@gmail.com>
2024-11-22 09:28:50 +0100

    Samuel peña

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,28 @@ import './bootstrap';
 
 import Alpine from 'alpinejs';
 
+import Sortable from 'sortablejs'; 
+
+
+
+// Inicializa Sortable en el contenedor que deseas hacer arrastrable
+document.addEventListener('DOMContentLoaded', function() {
+    const container = document.getElementById('sortable-container');
+    Sortable.create(container, {
+        animation: 150,
+        ghostClass: 'sortable-ghost', // Clase para el elemento que se está arrastrando
+        onEnd: function (evt) {
+            console.log('Elemento movido de ' + evt.oldIndex + ' a ' + evt.newIndex);
+            // Aquí puedes agregar lógica para guardar el nuevo orden en el servidor si es necesario
+        }
+    });
+});
+
+
+
+
+
+
 window.Alpine = Alpine;
 
 Alpine.start();
